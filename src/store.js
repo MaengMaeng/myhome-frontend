120cae8db40e0faf84e5df0432069bd3fefe7646
godsangin <leein3293@naver.com>
2019-08-27 17:36:16 +0900

    board modify, delete function are developing...

--- src/store.js
+++ src/store.js
@@ -4,17 +4,17 @@ import Vuex from 'vuex'
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
-  state:{
-    // server_ip:'http://10.3.144.218:8080',
-    // server_ip:'http://168.188.125.194:8080',
-    // server_ip:'http://ec2-52-79-126-1.ap-northeast-2.compute.amazonaws.com:8080',
-    server_ip:'http://192.168.31.65:8080',
-    isLogin:false,
-    user:{
-      u_mail:'',
-      u_name:'',
-      u_tel:'',
-      u_birth:''
+    state: {
+        // server_ip:'http://10.3.144.218:8080',
+        // server_ip:'http://168.188.125.194:8080',
+        server_ip: 'http://ec2-52-79-126-1.ap-northeast-2.compute.amazonaws.com:8080',
+        // server_ip:'http://192.168.31.65:8080',
+        isLogin: false,
+        user: {
+            u_mail: '',
+            u_name: '',
+            u_tel: '',
+            u_birth: ''
+        }
     }
-	}
 });
