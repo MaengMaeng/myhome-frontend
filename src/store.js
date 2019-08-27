26d471f0a9ef4583d2f6854adceb2ead507100f5
MaengMaeng <maybe8189@naver.com>
2019-08-27 17:30:01 +0900

    Home 사진 변경 / 게시판 조회수 삭제

--- src/store.js
+++ src/store.js
@@ -7,8 +7,8 @@ export const store = new Vuex.Store({
   state:{
     // server_ip:'http://10.3.144.218:8080',
     // server_ip:'http://168.188.125.194:8080',
-    // server_ip:'http://ec2-52-79-126-1.ap-northeast-2.compute.amazonaws.com:8080',
-    server_ip:'http://192.168.31.65:8080',
+    server_ip:'http://ec2-52-79-126-1.ap-northeast-2.compute.amazonaws.com:8080',
+    // server_ip:'http://localhost:8080',
     isLogin:false,
     user:{
       u_mail:'',
